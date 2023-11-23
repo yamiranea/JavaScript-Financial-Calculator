69eaa8375e515f2f2cc34f03e78404346d012cba
yamiranea <yamilarenea@gmail.com>
2023-11-23 15:35:32 +0100

    basic js done

---
+++ script.js
@@ -0,0 +1,23 @@
+let num0 = 0;
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+let num4 = 4;
+let num5 = 5;
+let num6 = 6;
+let num7 = 7;
+let num8 = 8;
+let num9 = 9;
+
+document.getElementById("key-reset").textContent = reset;
+document.getElementById("key-0").textContent = num0;
+document.getElementById("key-1").textContent = num1;
+document.getElementById("key-2").textContent = num2;
+document.getElementById("key-3").textContent = num3;
+document.getElementById("key-4").textContent = num4;
+document.getElementById("key-5").textContent = num5;
+document.getElementById("key-6").textContent = num6;
+document.getElementById("key-7").textContent = num7;
+document.getElementById("key-8").textContent = num8;
+document.getElementById("key-9").textContent = num9;
+
