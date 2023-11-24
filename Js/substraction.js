cd0a7b80bc8f021427787269860ec5695cc43b5b
yamiranea <yamilarenea@gmail.com>
2023-11-24 19:42:25 +0100

    division done

--- Js/substraction.js
+++ Js/substraction.js
@@ -3,6 +3,7 @@ document
   .addEventListener("click", function () {
     operation = (value1, value2) => {
       return parseFloat(value1) - parseFloat(value2);
+      updateDisplay();
     };
     prevValue = display;
     display = "0";
