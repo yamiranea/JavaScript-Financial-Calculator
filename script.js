53f75835e9efeae29d09e56ac91f88c35f0fe3ba
yamiranea <yamilarenea@gmail.com>
2023-11-24 09:40:02 +0100

    html fixed to improve js code using addevent listener instead of onclick

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-let display = 0;
-let num0 = 0;
-let num1 = 1;
-let num2 = 2;
-let num3 = 3;
-let num4 = 4;
-let num5 = 5;
-let num6 = 6;
-let num7 = 7;
-let num8 = 8;
-let num9 = 9;
+let display = "0";
 
-document.getElementById("display").textContent = display;
-document.getElementById("key-reset").textContent = reset;
-document.getElementById("key-0").textContent = num0;
-document.getElementById("key-1").textContent = num1;
-document.getElementById("key-2").textContent = num2;
-document.getElementById("key-3").textContent = num3;
-document.getElementById("key-4").textContent = num4;
-document.getElementById("key-5").textContent = num5;
-document.getElementById("key-6").textContent = num6;
-document.getElementById("key-7").textContent = num7;
-document.getElementById("key-8").textContent = num8;
-document.getElementById("key-9").textContent = num9;
-
-function key1() {
-    display
+function updateDisplay() {
+    document.getElementById("display").textContent = display;
 }
+
+function resetDisplay() {
+    display = "0";
+    updateDisplay();
+}
+
+function appendNumber (number) {
+    if (display === "0") {
+        display = number;
+    } else {
+        display += number;
+    }
+}
+
+updateDisplay();
+
+document.getElementById("key0").add
