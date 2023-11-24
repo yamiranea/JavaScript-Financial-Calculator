ad649fd7f383ca5ee4333c5aa0352916de7b7bb9
yamiranea <yamilarenea@gmail.com>
2023-11-24 14:57:47 +0100

    values 1 and 2 modificated

--- Js/substraction.js
+++ Js/substraction.js
@@ -2,7 +2,7 @@ document
   .getElementById("key-substraction")
   .addEventListener("click", function () {
     operation = (value1, value2) => {
-      return parseFloat(value2) - parseFloat(value1);
+      return parseFloat(value1) - parseFloat(value2);
     };
     prevValue = display;
     display = "0";
